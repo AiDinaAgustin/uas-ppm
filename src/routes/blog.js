49a1ee99d6bd00582183bca1a01a464d9075d537
AiDinaAgustin <aidinagustin2@gmail.com>
2024-02-01 20:44:39 +0700

    post update server

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -7,8 +7,8 @@ router.get('/', (req,res) => {
     res.status(200).json('Server on port 8000 and database is connected');
 });
 
-router.get('/:users', (req,res) => {
-    mysqlConnection.query('select * from user;', (error, rows, fields) => {
+router.get('/:posts', (req,res) => {
+    mysqlConnection.query('select * from post;', (error, rows, fields) => {
         if(!error) {
             res.json(rows);
         } else {
@@ -16,6 +16,45 @@ router.get('/:users', (req,res) => {
         }
     })
 })
+
+router.get('/:posts/:id', (req,res) => {
+    const {id} = req.params;
+    mysqlConnection.query('select * from post where id = ?;', [id], (error, rows, fields) => {
+        if (!error) {
+            res.json(rows);
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+router.post('/:posts', (req,res) => {
+    const {id, user_id, category_id, title, body} = req.body;
+    console.log(req.body);
+    mysqlConnection.query('insert into post(id, user_id, category_id, title, body) values (?,?,?,?,?);',
+    [id, user_id, category_id, title, body], (error, rows, fields) => {
+        if (!error) {
+            res.json({Status: 'Post saved'});
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+router.put('/:posts/:id', (req,res) => {
+    const {id, user_id, category_id, title, body} = req.body;
+    console.log(req.body);
+    mysqlConnection.query('update post set user_id = ?, category_id = ?, title = ?, body = ?, where id = ?;', 
+    [user_id, category_id, title, body, id], (error, rows, fields) =>{
+        if (!error) {
+            res.json({Status: 'Post Updated'});
+        } else {
+            console.log('error')
+        }
+    });
+});
+
+
 
 
 
